14e6c9195e837b21ea7b011495d2d0a0a86cbd37
Arash Paolo Kazemi Koohbanani <a.kazemikoohbanani@student.tudelft.nl>
2019-11-20 09:26:45 +0100

    Upgraded LwjGL to LwjGL3, added icon and logo to the screen

--- src/main/java/game/BubbleSpinner.java
+++ src/main/java/game/BubbleSpinner.java
@@ -1,8 +1,9 @@
 package game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -11,28 +12,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  * This class handles all the interactions
  * with the Game library.
  */
-public class BubbleSpinner extends ApplicationAdapter {
+public class BubbleSpinner extends Game {
     private transient SpriteBatch batch;
-    private transient Texture img;
+    private transient OrthographicCamera camera;
+    private transient Texture logo;
 
     @Override
     public void create() {
+        logo = new Texture("assets/logo.png");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch = new SpriteBatch();
-        img = new Texture("assets/badlogic.jpg");
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(logo,
+                Gdx.graphics.getWidth() / 2 - logo.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - logo.getHeight() / 2);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        logo.dispose();
     }
 }
