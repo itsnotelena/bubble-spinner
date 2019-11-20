51b6d38954e27e362a023ead77734af213b3c081
Victor van Wieringen <V.J.vanWieringen@student.tudelft.nl>
2019-11-20 19:03:42 +0100

    Add default theme

---
+++ src/main/java/game/LoginScreen.java
@@ -0,0 +1,72 @@
+package game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class LoginScreen extends ScreenAdapter {
+    private static final float WORLD_WIDTH = 480;
+    private static final float WORLD_HEIGHT = 320;
+    private transient Stage stage;
+    private transient Game game;
+
+    public LoginScreen(Game game) {
+        this.game = game;
+    }
+    public void show() {
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        stage = new Stage(new ScreenViewport());
+        Label title = new Label("Title Screen", skin,"big-black");
+        title.setAlignment(Align.center);
+        title.setY(Gdx.graphics.getHeight()*2/3);
+        title.setWidth(Gdx.graphics.getWidth());
+        stage.addActor(title);
+
+        TextButton playButton = new TextButton("Play!", skin);
+        playButton.setWidth(Gdx.graphics.getWidth()/2);
+        playButton.setPosition(Gdx.graphics.getWidth()/2-playButton.getWidth()/2,Gdx.graphics.getHeight()/2-playButton.getHeight()/2);
+        playButton.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new LoginScreen(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        stage.addActor(playButton);
+    }
+
+    public void resize(int w, int h){
+        stage.getViewport().update(w,h,true);
+    }
+
+    public void render(float delta){
+
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        stage.dispose();
+    }
+}
