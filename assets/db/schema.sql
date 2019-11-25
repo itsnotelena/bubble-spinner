85913a84e2851111dee8f170a8bce3cb6a42a95c
ZiadNawar <ziadahmadsaadsolimannawar@student.tudelft.nl>
2019-11-25 14:31:43 +0100

    Commit assets for database scheme

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -1,5 +1,19 @@
 CREATE TABLE IF NOT EXISTS users (
 	username VARCHAR(64) PRIMARY KEY,
-	email VARCHAR(64) NOT NULL,
-	password VARCHAR(64) NOT NULL
+	email VARCHAR(64) ,
+	password VARCHAR(64)
+);
+CREATE TABLE IF NOT EXISTS score (
+	username VARCHAR(64) PRIMARY KEY,
+	scoreW int(64) NOT NULL,
+	scoreA int(64) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS games (
+	username VARCHAR(64) NOT NULL,
+	gamesP int(64) NOT NULL,
+	highestL int(64) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS badges (
+	username VARCHAR(64) NOT NULL,
+	award VARCHAR(64) NOT NULL
 );
