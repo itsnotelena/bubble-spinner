52de835ec4a399e4791b74e54ccdc94d459b6236
Arash Paolo Kazemi Koohbanani <a.kazemikoohbanani@student.tudelft.nl>
2019-12-02 19:24:07 +0100

    Added aiming direction, a line shows the direction and the first reflection of the current mouse position on the screen

--- src/main/java/game/ui/GameScreen.java
+++ src/main/java/game/ui/GameScreen.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import game.BubbleSpinner;
@@ -22,6 +25,7 @@ public class GameScreen implements Screen {
     private transient BubbleSpinnerController bubbleSpinnerController;
     private transient Date startingTime;
     private transient BitmapFont timerFont;
+    private transient ShapeRenderer shapeRenderer;
 
     /**
      * This is Screen where the game is played.
@@ -41,6 +45,8 @@ public class GameScreen implements Screen {
         timerFont = new BitmapFont();
         timerFont.setColor(Color.BLACK);
         timerFont.getData().setScale(2);
+
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
@@ -63,6 +69,7 @@ public class GameScreen implements Screen {
                 7 * Gdx.graphics.getHeight() / 8
         );
         game.batch.end();
+        drawArrow();
 
         stage.act();
         stage.draw();
@@ -118,5 +125,72 @@ public class GameScreen implements Screen {
         String timer = (minutes < 10 ? "0" : "") + minutes + ":"
                         + (seconds < 10 ? "0" : "") + seconds;
         return timer;
+    }
+
+    /**
+     * Draw an arrow based on the current
+     * position of the mouse.
+     */
+    private void drawArrow() {
+        final int bubbleSize = 64;
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.setColor(Color.BLACK);
+
+        // Calculate Stage Coordinates for mouse
+        Vector2 pos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+        stage.screenToStageCoordinates(pos);
+
+        Vector2 intersection = new Vector2(-1, -1);
+        if (pos.x < Gdx.graphics.getWidth() / 2) {                      // Check left or right side
+            Intersector.intersectLines(Gdx.graphics.getWidth() / 2, // Bubble center X
+                    Gdx.graphics.getHeight() - bubbleSize / 2,      // Bubble center Y
+                    pos.x,                                              // Mouse position X
+                    pos.y,                                              // Mouse position Y
+                    bubbleSize / 2,                                 // Left Origin + half bubble X
+                    0,                                              // Left Origin Y
+                    bubbleSize / 2,                                 // Left wall + half bubble X
+                    Gdx.graphics.getHeight(),                           // Left wall Height
+                    intersection                                        // Intersection vector
+            );
+        } else {
+            Intersector.intersectLines(Gdx.graphics.getWidth() / 2, // Bubble center X
+                    Gdx.graphics.getHeight() - bubbleSize / 2,      // Bubble center Y
+                    pos.x,                                              // Mouse position X
+                    pos.y,                                              // Mouse position Y
+                    1280 - (bubbleSize / 2),                        // Right Origin - half bubble X
+                    0,                                              // Right Origin Y
+                    1280 - (bubbleSize / 2),                        // Right wall - half bubble X
+                    Gdx.graphics.getHeight(),                           // Right wall Height
+                    intersection                                        // Intersection vector
+            );
+        }
+        shapeRenderer.line(Gdx.graphics.getWidth() / 2,             // Bubble center X
+                Gdx.graphics.getHeight() - bubbleSize / 2,          // Bubble center Y, to:
+                intersection.x,                                        // Intersection X
+                intersection.y                                         // Intersection Y
+        );
+
+        float m = -1 * (intersection.y - (Gdx.graphics.getHeight() - (bubbleSize / 2)))  // Delta Y
+                    / (intersection.x - (Gdx.graphics.getWidth() / 2));                  // Delta X
+
+        if (pos.x < Gdx.graphics.getWidth() / 2) {              // Check left or right side
+            float y = m * 1280 + intersection.y;                // Calculate y intersection
+            shapeRenderer.line(intersection.x,                  // Intersection X
+                    intersection.y,                             // Intersection Y
+                    1280,                                   // Right wall X
+                    y                                           // Right wall Y
+            );
+        } else {
+            float q = intersection.y - m * intersection.x;      // Calculate q intersection
+            shapeRenderer.line(intersection.x,                  // Intersection X
+                    intersection.y,                             // Intersection Y
+                    0,                                      // Left wall X
+                    q                                           // Left wall Y
+            );
+        }
+
+        shapeRenderer.end();
     }
 }
