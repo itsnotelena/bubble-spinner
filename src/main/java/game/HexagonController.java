a25332ead76e18317006f4bc43d2aa895d602ced
ana <a.tatabitovska@student.tudelft.nl>
2020-01-14 14:59:01 +0100

    Updating

--- src/main/java/game/HexagonController.java
+++ src/main/java/game/HexagonController.java
@@ -2,8 +2,12 @@ package game;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class HexagonController {
 
@@ -24,8 +28,64 @@ public class HexagonController {
         //Max is the Difficulty of the Game
         this.bubbleFactory.addAllTextures(4);
         this.bubbles = new ArrayList<>();
-        this.bubbles.add(bubbleFactory.createBubble().center());
-        stage.addActor(this.bubbles.get(0));
+        BubbleActor center = bubbleFactory.next().center();
+        stage.addActor(center);
+
+        int bubTotal = 18;
+
+        for (int i = 0; i < bubTotal; i++) {
+            BubbleActor bub = bubbleFactory.next();
+            bubbles.add(bub);
+            stage.addActor(bub);
+        }
+
+        int lastneigh = 7;
+        int temp;
+
+        File file = new File("assets\\hexagon_easy.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        sc.useDelimiter(", |\n|\r");
+        sc.useLocale(Locale.US);
+        for (BubbleActor bubble: bubbles) {
+            if (!(sc.hasNext())) {
+                break;
+            }
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            bubble.center().shiftX(true, (float)x);
+            bubble.shiftY(true, (float)y);
+            sc.next();
+        }
+        int num2 = 0;
+        for(BubbleActor a: bubbles){
+            int num = 0;
+            for(BubbleActor b: bubbles){
+
+                if(!a.collide(b)) {
+                    float xa = a.getX();
+                    float ya = a.getY();
+                    float xb = b.getX();
+                    float yb = b.getY();
+                    float lenx = (float) Math.pow((xa - xb), 2);
+                    float leny = (float) Math.pow((ya - yb), 2);
+                    float len = (float) Math.sqrt(lenx + leny);
+                    if(len<=120.0 && len>=65.0){
+                        a.neighbours.add(b);
+                        System.out.println("Added neighbour num "+ num);
+                        num++;
+
+                    }
+
+                }
+            }
+            System.out.println("Done with bubble "+num2);
+            num2++;
+        }
     }
 
     /**
@@ -47,8 +107,50 @@ public class HexagonController {
         return false;
     }
 
-    public List<BubbleActor> getBubbles() {
-        return bubbles;
+    /**
+     * Recursive method to find how many neighbouring
+     * bubbles will be popped after hit.
+     * @return number of bubbles
+     * @param hit bubble whose neighbours we check for
+     *            matching color id.
+     */
+    public static int bubblePop(BubbleActor hit, int counter){
+//        int id = hit.getColorID();
+//        for(BubbleActor candidate: hit.neighbours){
+//            if(candidate.getColorId()==id){
+//                counter++;
+//                bubblePop(candidate, counter);
+//            }
+//        }
+        return counter;
     }
 
+    /**
+     * Method calculates score based on how many
+     * bubbles have been popped.
+     * @return the score due to the hit
+     * @param hitter is the bubble shot under the user's command
+     */
+    public int calculateScore(BubbleActor hitter){
+        int num = 0;
+        for(BubbleActor hit: bubbles){
+            if(hit.collide(hitter)){
+                num = num + bubblePop(hit, 0);
+            }
+        }
+        return formula(num);
+    }
+
+    /**
+     * Helper method to help with
+     * score calculation
+     * @param num is number of bubbles popped
+     */
+    public int formula(int num){
+        if(num==3){
+            return 5;
+        } else {
+            return (int)(1.5 * formula(num-1));
+        }
+    }
 }
