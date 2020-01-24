fcee15382ad8bdf8dbe36fdbeec4696ca819215c
ana <a.tatabitovska@student.tudelft.nl>
2020-01-24 12:00:04 +0100

    Set up tests part 2.

--- src/test/java/game/HexagonControllerTest.java
+++ src/test/java/game/HexagonControllerTest.java
@@ -1,39 +1,39 @@
 package game;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+
 
 class HexagonControllerTest {
 
     HexagonController controller;
+
 
     @BeforeEach
     void setUp() {
         controller = new HexagonController(Mockito.mock(Stage.class), 0);
     }
 
-    @Test
-    void getBubbles() {
-    }
-
-    @Test
-    void positionBubble() {
-    }
+//    @Test
+//    void positionBubble() {
+//        BubbleFactory fac = Mockito.mock(BubbleFactory.class);
+//        Texture tex = Mockito.mock(Texture.class);
+//        controller.setBubbleFactory(fac);
+//        Mockito.when(fac.createBubble()).thenReturn(new BubbleActor(tex, null , 0, 1));
+//
+//        controller.positionBubble(0,1);
+//        Assertions.assertThat(controller.getBubbles().get(1).getPosition().x).isEqualTo(0);
+//        Assertions.assertThat(controller.getBubbles().get(1).getPosition().y).isEqualTo(1);
+//    }
 
     @Test
     void drawGrid() {
-    }
-
-    @Test
-    void setBuilder() {
-    }
-
-    @Test
-    void getBuilder() {
     }
 
     @Test
@@ -58,7 +58,17 @@ class HexagonControllerTest {
 
     @Test
     void formula1() {
+        Assertions.assertThat(controller.formula(3)).isEqualTo(5);
+    }
 
+    @Test
+    void formula2() {
+        Assertions.assertThat(controller.formula(4)).isEqualTo(7);
+    }
+
+    @Test
+    void formula3() {
+        Assertions.assertThat(controller.formula(2)).isEqualTo(0);
     }
 
     @Test
