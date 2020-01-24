44642248311b54274f8d752e723bd3f2d4a5833e
Victor van Wieringen <V.J.vanwieringen@student.tudelft.nl>
2020-01-24 12:13:55 +0100

    Add a bunch of tests:

--- src/test/java/game/BubbleSpinnerControllerTest.java
+++ src/test/java/game/BubbleSpinnerControllerTest.java
@@ -44,7 +44,7 @@ public class BubbleSpinnerControllerTest {
         shooter = Mockito.mock(Shooter.class);
         hexagonController = Mockito.mock(HexagonController.class);
         bubble = Mockito.mock(BubbleActor.class);
-        Mockito.doNothing().when(hexagonController);
+        //Mockito.doNothing().when(hexagonController);
         Mockito.doNothing().when(hexagonController).drawGrid();
         Mockito.doNothing().when(shooter).initialize(Mockito.any());
         Mockito.when(shooter.current()).thenReturn(bubble);
